a55ce485d9014adbeb269f55a1a64b2ddf8f51c1
Kaya Kaan Tuna <tunakayakaan@gmail.com>
2024-05-15 22:22:00 +0300

    Added `url` and `search_query` arg to the code.
    ## Problems
    
    - LinkedIn blocks the request that comes from our program.
    
    - We can use `selenium webdriver`. But this will convert this project into a live bot.
    
    - `Webdriver++` is selenium webdriver's cpp version. It can be used if we want to go for that path.
    
    Signed-off-by: Kaya Kaan Tuna <tunakayakaan@gmail.com>

--- inc/HtmlParser.hpp
+++ inc/HtmlParser.hpp
@@ -11,5 +11,4 @@ public:
     virtual void parseHTML(const std::string& htmlContent) = 0;
     virtual void addSearchQuery(const std::string& query) = 0;
     virtual const std::vector<std::string>& getResults() const = 0;
-    virtual int countQueryOccurrences(const std::string& query) = 0;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,27 @@
 #include "LibXmlHtmlParser.hpp"
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
     CurlHttpClient client;
     LibXmlHtmlParser parser;
+
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <url> <search_query>" << std::endl;
+        return 1;
+    }
+
+    std::string url = argv[1];
+    std::string searchQuery = argv[2];
+
+    std::cout << "Searching URL: " << url << std::endl;
+    
     
     client.enable();
-    std::string result = client.getRequest("https://www.ets.org/gre/test-takers/general-test/register/fees.html");
-    //std::cout << result << std::endl;
-    parser.addSearchQuery("Score Review for Analytical Writing measure");
-    parser.parseHTML(result);
+    std::string result = client.getRequest(url);
+    std::cout << result << std::endl;
+    parser.addSearchQuery(searchQuery);
+    parser.parseHTML(result); 
     const std::vector<std::string>& results_parser = parser.getResults();
 
     for (const std::string& results : results_parser) {
